72ca3e4367a22a63e529d98eff79d75582746aca
syunnjack <syunnda1@yahoo.co.jp>
2026-08-04 15:05:03 +0900

    Add GA4 tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
     <html
       lang="ja"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    ><head><script async src="https://www.googletagmanager.com/gtag/js?id=G-X14H01QGTL"></script><script dangerouslySetInnerHTML={{__html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-X14H01QGTL');`}} /></head>
       <body className="min-h-full flex flex-col">
         <header className="border-b border-slate-200 bg-white">
           <div className="mx-auto flex max-w-4xl items-center justify-between px-4 py-3">
